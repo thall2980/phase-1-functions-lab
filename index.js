4b8e9660be52921ba1b147e402c70555121bde7a
Tyler Hall <thall2980@gmail.com>
2022-08-29 22:50:11 -0400

    commit tree a5e1a30c360571bce05c0095b9a5c2ccb0e8f8e7

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+
+
+function distanceFromHqInBlocks(someValue) {
+    if (someValue > 42) {
+        return someValue - 42;
+    } else {
+        return 42- someValue;
+    }
+}
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+        return (start - destination) * 264;
+    } else {
+        return (destination - start) * 264;
+    } 
+    
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination)
+    if (distance <= 400) {
+        return 0
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * .02;
+    } else if (distance > 2000 && distance < 2500) {
+        return 25
+    } else {
+        return "cannot travel that far"
+    }
+}
